644a32a6862f0b78ec8f0ffc9fc3736122df69f4
Anika Rahman Nira <anikarahman960@gmail.com>
2021-04-29 11:28:27 +0600

    .........

--- Admin/js/emptyValidation.js
+++
@@ -1,8 +0,0 @@
-function emptyValidation() {
-  var fname = document.getElementById("firstname").value;
-  var email = document.getElementById("email").value;
-  if (fname == "" || email == "") {
-    alert("Fill out name")
-    return false;
-  }
-}
